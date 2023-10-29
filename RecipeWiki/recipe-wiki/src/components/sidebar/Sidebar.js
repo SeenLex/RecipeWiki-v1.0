f817dc850db2b8b521d051a2ffa7d54451c573c7
SeenLex <draghicidenis@yahoo.com>
2023-10-29 18:04:50 +0200

    filter

--- RecipeWiki/recipe-wiki/src/components/sidebar/Sidebar.js
+++ RecipeWiki/recipe-wiki/src/components/sidebar/Sidebar.js
@@ -17,64 +17,75 @@ export const Sidebar = () => {
     }));
   };
 
+  const toggleSidebarDropdown = () => {
+    setDropdownState((prevState) => ({
+      ...prevState,
+      sidebar: !prevState.sidebar,
+    }));
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar-items"></div>
       <MyFavourites />
       <div className="sidebar-title">
-        <b>Filter</b>
+        <button className="sidebar-title-Button" onClick={toggleSidebarDropdown}>
+          <b>Filter</b>
+        </button>
       </div>
-      <div className="meals">
-        <div>
-          <button onClick={() => toggleDropdown("breakfast")} className="breakfast">
-            Breakfast
-          </button>
-          {dropdownState.breakfast && (
-            <div className="type-of-meals">
-              {/* Content for the Breakfast dropdown */}
-              {/* Add your content here */}
-            </div>
-          )}
-        </div>
+      {dropdownState.sidebar && (
+        <div className="meals">
+          <div>
+            <button onClick={() => toggleDropdown("breakfast")} className="breakfast">
+              Breakfast
+            </button>
+            {dropdownState.breakfast && (
+              <div className="type-of-meals">
+                {/* Content for the Breakfast dropdown */}
+                {/* Add your content here */}
+              </div>
+            )}
+          </div>
 
-        <div>
-          <button onClick={() => toggleDropdown("brunch")} className="brunch">
-            Brunch
-          </button>
-          {dropdownState.brunch && (
-            <div className="type-of-meals">
-              {/* Content for the Brunch dropdown */}
-              {/* Add your content here */}
-            </div>)
-          }
-        </div>
+          <div>
+            <button onClick={() => toggleDropdown("brunch")} className="brunch">
+              Brunch
+            </button>
+            {dropdownState.brunch && (
+              <div className="type-of-meals">
+                {/* Content for the Brunch dropdown */}
+                {/* Add your content here */}
+              </div>
+            )}
+          </div>
 
-        <div>
-          <button onClick={() => toggleDropdown("lunch")} className="lunch">
-            Lunch
-          </button>
-          {dropdownState.lunch && (
-            <div className="type-of-meals">
-              {/* Content for the Lunch dropdown */}
-              {/* Add your content here */}
-            </div>)
-          }
-        </div>
+          <div>
+            <button onClick={() => toggleDropdown("lunch")} className="lunch">
+              Lunch
+            </button>
+            {dropdownState.lunch && (
+              <div className="type-of-meals">
+                {/* Content for the Lunch dropdown */}
+                {/* Add your content here */}
+              </div>
+            )}
+          </div>
 
-        <div>
-          <button onClick={() => toggleDropdown("dinner")} className="dinner">
-            Dinner
-          </button>
-          {dropdownState.dinner && (
-            <div className="type-of-meals">
-              <div className="carne">sadada</div>
-              <div className="carne">sadada</div>
-              <div className="carne">sadada</div>
-              <div className="carne">sadada</div>
-            </div>)
-          }
+          <div>
+            <button onClick={() => toggleDropdown("dinner")} className="dinner">
+              Dinner
+            </button>
+            {dropdownState.dinner && (
+              <div className="type-of-meals">
+                <div className="carne">sadada</div>
+                <div className="carne">sadada</div>
+                <div className="carne">sadada</div>
+                <div className="carne">sadada</div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
